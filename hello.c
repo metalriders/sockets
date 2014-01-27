d904edce243ffaafa30fe094061bb732b7a01aaf
Daniel <daniboy307@outlook.com>
2014-01-26 21:43:59 -0600

    Update hello.c
    Archivo después de los comflictos

--- hello.c
+++ hello.c
@@ -4,10 +4,10 @@ void main(void) {
 	printf("Hola Mundo\n");
 }
 
-void funcion1(void) {
-	printf("Funcion 1\n");
-}
-
 void funcion2(void) {
 	printf("Funcion 2\n")
+}
+
+void funcion1(void) {
+	printf("Funcion 1\n");
 }
